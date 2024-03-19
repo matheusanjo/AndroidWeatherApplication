e78954e7af0efc9279cc042c5113ec985d6cc34c
Matheus Anjo <matheus.anjo.andre@gmail.com>
2024-03-19 20:43:05 -0300

    weather fixes

--- app/src/main/java/com/infinityandroid/weatherapp/data/WeatherData.kt
+++ app/src/main/java/com/infinityandroid/weatherapp/data/WeatherData.kt
@@ -11,7 +11,7 @@ data class CurrentLocation(
     val location: String = "Choose your location",
     val latitude: Double? = null,
     val longitude: Double? = null
-): WeatherData()
+) : WeatherData()
 
 data class CurrentWeather(
     val icon: String,
@@ -19,7 +19,14 @@ data class CurrentWeather(
     val wind: Float,
     val humidity: Int,
     val chanceOfRain: Int
-): WeatherData()
+) : WeatherData()
+
+data class Forecast(
+    val time: String,
+    val temperature: Float,
+    val feelsLikeTemperature: Float,
+    val icon: String
+) : WeatherData()
 
 private fun getCurrentDate(): String {
     val currentDate = Date()
